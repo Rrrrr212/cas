f217d2f891a7e1e0fd167c8e8c3531c0a11930c8
Rrrr <2823785414@qq.com>
2026-06-03 11:33:34 +0000

    3.7.4

--- support/cas-server-support-hazelcast-ticket-registry/src/main/java/org/apereo/cas/config/CasHazelcastTicketRegistryAutoConfiguration.java
+++ support/cas-server-support-hazelcast-ticket-registry/src/main/java/org/apereo/cas/config/CasHazelcastTicketRegistryAutoConfiguration.java
@@ -8,7 +8,6 @@ import org.apereo.cas.hz.HazelcastMapCustomizer;
 import org.apereo.cas.ticket.TicketCatalog;
 import org.apereo.cas.ticket.TicketDefinition;
 import org.apereo.cas.ticket.catalog.CasTicketCatalogConfigurationValuesProvider;
-import org.apereo.cas.ticket.registry.HazelcastClusterHealthIndicator;
 import org.apereo.cas.ticket.registry.HazelcastTicketDocument;
 import org.apereo.cas.ticket.registry.HazelcastTicketRegistry;
 import org.apereo.cas.ticket.registry.MapAttributeValueExtractor;
@@ -30,8 +29,6 @@ import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.boot.health.autoconfigure.contributor.ConditionalOnEnabledHealthIndicator;
-import org.springframework.boot.health.contributor.HealthIndicator;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
@@ -148,14 +145,5 @@ public class CasHazelcastTicketRegistryAutoConfiguration {
     @Lazy(false)
     public TicketRegistryCleaner ticketRegistryCleaner() {
         return NoOpTicketRegistryCleaner.getInstance();
-    }
-
-    @Bean
-    @ConditionalOnMissingBean(name = "hazelcastClusterHealthIndicator")
-    @ConditionalOnEnabledHealthIndicator("hazelcastClusterHealthIndicator")
-    @RefreshScope(proxyMode = ScopedProxyMode.DEFAULT)
-    public HealthIndicator hazelcastClusterHealthIndicator(
-        @Qualifier("casTicketRegistryHazelcastInstance") final HazelcastInstance casTicketRegistryHazelcastInstance) {
-        return new HazelcastClusterHealthIndicator(casTicketRegistryHazelcastInstance);
     }
 }
