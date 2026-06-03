d48645461aaaed2dbbcf4e9f15dd659926d60d6b
Rrrr <2823785414@qq.com>
2026-06-03 10:00:28 +0000

    3.6.3

--- support/cas-server-support-webauthn-core/src/test/java/org/apereo/cas/util/AbstractCasMockitoTest.java
+++
@@ -1,8 +0,0 @@
-package org.apereo.cas.util;
-
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
-
-@ExtendWith(MockitoExtension.class)
-public abstract class AbstractCasMockitoTest {
-}
